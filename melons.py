af61fb349261a2b1ff0287c5ca175d0858646cd5
Taiyo Scanlon-Kimura <taiyo.scanlon-kimura@fulbrightmail.org>
2023-04-13 15:37:38 -0700

    init commit

---
+++ melons.py
@@ -0,0 +1,41 @@
+"""Melon and Squash classes."""
+
+import robots
+
+
+class Melon:
+    """Melon."""
+
+    def __init__(self, melon_type):
+        """Initialize melon.
+
+        melon_type: type of melon being built.
+        """
+
+        self.melon_type = melon_type
+        self.weight = 0.0
+        self.color = None
+        self.stickers = []
+
+    def prep(self):
+        """Prepare the melon."""
+
+
+
+    def __str__(self):
+        """Print out information about melon."""
+
+        if self.weight <= 0:
+            return self.melon_type
+        else:
+            return f"{self.color} {self.weight:.2f} lbs {self.melon_type}"
+
+
+class Squash(Melon):
+    """Winter squash."""
+
+    def __init__(self, melon_type):
+        super().__init__(melon_type)
+
+    def __paint__(self):
+        robots.painterbot.paint(self)
